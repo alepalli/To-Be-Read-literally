2645e668838f17bad8603434d7448d583bf882a6
Alessia Pallavera <alessia.pallavera01@gmail.com>
2025-01-27 15:00:23 +0100

    Initial commit

---
+++ src/main/java/com/alepalli/To_Be_Read_literally/ToBeReadLiterallyApplication.java
@@ -0,0 +1,13 @@
+package com.alepalli.To_Be_Read_literally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToBeReadLiterallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToBeReadLiterallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alepalli/To_Be_Read_literally/ToBeReadLiterallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alepalli.To_Be_Read_literally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToBeReadLiterallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
